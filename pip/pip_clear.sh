4df36a6ffda082e8819ffba612efb133ce3f4155
Furzmund <20493918+Furzmund@users.noreply.github.com>
2024-08-30 09:57:44 -0600

    pip_clear creates a backup list

--- pip/pip_clear.sh
+++ pip/pip_clear.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+declare DATE_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
+declare BACKUP_FILE="pip_backup_${DATE_TIME}.txt"
+
+echo "Creating backup list"
+pip freeze > ${BACKUP_FILE}
+echo "  Backup: ${BACKUP_FILE}"
+echo "  To restore, run: pip install -r ${BACKUP_FILE}"
+echo
+
 echo "Clearing pip installs"
 pip freeze | grep -v "^-e" | xargs pip uninstall -y
 echo "Done"
+echo
+
 pip list
 exit 0
